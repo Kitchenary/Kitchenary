79b989c6a17c2ef90740b5703faf12f61dc6d774
Shuotian Chen <shuoc@microsoft.com>
2018-07-23 13:14:11 -0700

    Add Edamam Client

---
+++ Kitchenary/EdamamService/EdamamClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace EdamamService
+{
+    public class EdamamClient
+    {
+        private string appId;
+        private string appKey;
+        private HttpClient Client {get; set;}
+
+        public EdamamClient(string appId, string appKey)
+        {
+            Client = new HttpClient();
+            this.appId = appId;
+            this.appKey = appKey;
+        }
+
+        public async Task<string> SearchRecipes(string queryString)
+        {
+            UriBuilder uri = new UriBuilder("https://api.edamam.com/search");
+            uri.Query = $"app_id={appId}&app_key={appKey}&q={queryString}";
+            var response = await Client.GetAsync(uri.ToString());
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ Kitchenary/EdamanServiceTests/EdamamClientTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EdamamService;
+
+
+namespace EdamamServiceTests
+{
+    [TestClass]
+    public class EdamamClientTest
+    {
+        EdamamClient client;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            client = new EdamamClient("b14df9ff", "46f071168e3ab576ca2144ea74109b37");
+        }
+
+        [TestMethod]
+        public async Task SearchRecipesTest()
+        {
+            var stuff = await client.SearchRecipes("chicken");
+        }
+    }
+}
