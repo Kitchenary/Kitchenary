a68edab15a82e4bc3340fec3c77ae6d3e362e241
Shuotian Chen <shuoc@microsoft.com>
2018-07-24 14:07:01 -0700

    Hook up recommendations

--- Kitchenary/Kitchenary/Controllers/RecommendationsController.cs
+++ Kitchenary/Kitchenary/Controllers/RecommendationsController.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Recommendation;
+using EdamamService;
+using System.Threading.Tasks;
 
 namespace Kitchenary.Controllers
 {
     public class RecommendationsController : Controller
     {
         // GET: Recommendations
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
             var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
             ViewBag.Name = userClaims?.FindFirst("name")?.Value;
 
-            return View();
+            string[] foods = { "pork", "bread", "peppers", "sugar", "corn" };
+            string[] pantryList = { "potatoes", "pork" };
+            IngredientList userPreferences = new IngredientList(foods);
+            IngredientList pantry = new IngredientList(pantryList);
+            List<Tuple<Recipe, double>> recs = await Recommender.GetRecommendations(userPreferences, pantry);
+
+            return View(recs);
         }
     }
 }
