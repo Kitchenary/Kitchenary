9ce3492fb587b6054218c9f95412d27b2f6c34c3
Shuotian Chen <shuoc@microsoft.com>
2018-07-24 13:11:55 -0700

    Improve preferences

--- Kitchenary/Kitchenary/Controllers/PreferencesController.cs
+++ Kitchenary/Kitchenary/Controllers/PreferencesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Kitchenary.Models;
 
 namespace Kitchenary.Controllers
 {
@@ -14,6 +15,12 @@ namespace Kitchenary.Controllers
             var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
             ViewBag.Name = userClaims?.FindFirst("name")?.Value;
 
+            return View();
+        }
+
+        public ActionResult Create(PreferencesModel model)
+        {
+            Models.PreferencesModel myModel = model;
             return View();
         }
     }
