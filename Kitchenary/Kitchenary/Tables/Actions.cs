00486d712185c562b71d339420a899c161a306c4
tinafanms <tinafan@microsoft.com>
2018-07-24 15:56:05 -0700

    Add azure table code for preference and pantry tables

---
+++ Kitchenary/Kitchenary/Tables/Actions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.Azure;
+using System.Collections.Generic;
+
+namespace Kitchenary
+{
+    public class TableActions
+    {
+        public static void AddRow (string tableName, TableEntity myTableEntity)
+        {
+            string storageAccountName = ConfigurationManager.AppSettings["StorageAccount"];
+            string storageAccountKey = ConfigurationManager.AppSettings["StorageKey"];
+            CloudStorageAccount storageAccount = new CloudStorageAccount(new StorageCredentials(storageAccountName,storageAccountKey), true);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable myTable = tableClient.GetTableReference(tableName);
+            myTable.CreateIfNotExists();
+            TableOperation insertOperation = TableOperation.InsertOrReplace(myTableEntity);
+            myTable.Execute(insertOperation);
+        }
+
+        public static IEnumerable<TableEntity> GetResult (string tableName,string user)
+        {
+            string storageAccountName = ConfigurationManager.AppSettings["StorageAccount"];
+            string storageAccountKey = ConfigurationManager.AppSettings["StorageKey"];
+            CloudStorageAccount storageAccount = new CloudStorageAccount(new StorageCredentials(storageAccountName, storageAccountKey), true);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable myTable = tableClient.GetTableReference(tableName);
+            //build filter string
+            string myTableFilter = TableQuery.GenerateFilterCondition("PrimaryKey", QueryComparisons.Equal, user);
+            TableQuery<TableEntity> tableQuery = new TableQuery<TableEntity>().Where(myTableFilter);
+            return myTable.ExecuteQuery(tableQuery);
+
+        }
+
+
+    }
+}
---
+++ Kitchenary/Kitchenary/Tables/TableEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+
+namespace TableClients
+{
+	public class PreferenceEntity : TableEntity
+	{
+		public PreferenceEntity(string user, string userId)
+		{
+			this.PartitionKey = user;
+			this.RowKey = userId;
+		}
+		public PreferenceEntity()
+		{ }
+		public string[] dietPreference { get; set; }
+		public string[] healthPreference { get; set; }
+        public string[] otherPreference { get; set; }
+        
+    }
+
+    public class PantryEntity : TableEntity
+    {
+        public PantryEntity(string user, string ingredientName)
+        {
+            this.PartitionKey = user;
+            this.RowKey = ingredientName;
+        }
+        public PantryEntity()
+        { }
+       
+        public string foodCategory { get; set; }
+
+		public string quantity { get; set; }
+
+		public DateTime ExpirationTime { get; set; }
+	}
+
+    
+}
